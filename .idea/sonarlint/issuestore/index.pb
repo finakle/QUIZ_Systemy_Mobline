8428c3f5b2368e71470f070525cebe951a7c042c
finakle <justynaputko@interia.pl>
2021-11-12 15:43:21 +0100

    poszerzenie funkcjonalnosci aplikacji

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+`
+0app/src/main/java/com/example/quiz/Question.java,c\9\c97244618e27ee7d3581fe78892ff4adefa63ba0
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.quiz;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import lombok.SneakyThrows;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,16 +19,100 @@ public class MainActivity extends AppCompatActivity {
     private Button falseButton;
     private Button nextButton;
     private TextView questionTextView;
+    private Button promptButton;
+    private boolean answerWasShown;
+
+    public static final String KEY_EXTRA_ANSWER = "pl.edu.pb.wi.quiz.correctAnswer";
+    private static final String QUIZ_TAG= "MainActivity";
+    private static final String KEY_CURRENT_INDEX = "currentIndex";
+    private static final int REQUEST_CODE_PROMPT = 0;
+
+
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda: onSaveInstanceState");
+        outState.putInt(KEY_CURRENT_INDEX, currentIndex);
+    }
+
+
+
+
+    private Question[] questions = new Question[] {
+            new Question(R.string.q_activity, false),
+            new Question(R.string.q_find_resources, false),
+            new Question(R.string.q_listener, false),
+            new Question(R.string.q_resources, false),
+            new Question(R.string.q_version, false),
+
+
+    };
+
+    private int currentIndex = 0;
+
+
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {return; }
+        if (requestCode == REQUEST_CODE_PROMPT){
+            if (data == null) {return; }
+            answerWasShown = data.getBooleanExtra(PromptActivity.KEY_EXTRA_ANSWER_SHOWN, false);
+        }
+    }
+
+
+
+    private void checkAnswerCorrectness(boolean userAnswer) {
+        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+        int resultMessageId = 0;
+        if (answerWasShown) {
+            resultMessageId = R.string.answer_was_shown;
+        } else {
+            if (userAnswer == correctAnswer) {
+                resultMessageId = R.string.correct_answer;
+            } else {
+                resultMessageId = R.string.incorrect_answer;
+            }
+        }
+        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void setNextQuestion(){
+        //currentIndex = (currentIndex + 1)%questions.length;
+        questionTextView.setText(questions[currentIndex].getQuestionId());
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onCreate");
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState !=null){
+            currentIndex = savedInstanceState.getInt(KEY_CURRENT_INDEX);
+        }
+
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
+        promptButton = findViewById(R.id.promptbutton);
         questionTextView = findViewById(R.id.question_text_view);
+
+        promptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PromptActivity.class);
+                boolean correctAnswer = questions[currentIndex].isTrueAnswer();
+                intent.putExtra(KEY_EXTRA_ANSWER,correctAnswer);
+                startActivityForResult(intent, REQUEST_CODE_PROMPT);
+                startActivityForResult(intent, REQUEST_CODE_PROMPT);
+            }
+        });
+
 
         trueButton.setOnClickListener( new View.OnClickListener() {
             @Override
@@ -44,42 +133,50 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 currentIndex = (currentIndex + 1)%questions.length;
+                answerWasShown = false;
                 setNextQuestion();
             }
         });
+
+
+
         setNextQuestion();
-
-
     }
 
 
-    private Question[] questions = new Question[] {
-            new Question(R.string.q_activity, false),
-            new Question(R.string.q_find_resources, false),
-            new Question(R.string.q_listener, false),
-            new Question(R.string.q_resources, false),
-            new Question(R.string.q_version, false),
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onResume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onPause");
+    }
 
-    };
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onStop");
+    }
 
-    private int currentIndex = 0;
-
-    private void checkAnswerCorrectness(boolean userAnswer) {
-        boolean correctAnswer = questions[currentIndex].isTrueAnswer();
-        int resultMessageId = 0;
-        if (userAnswer == correctAnswer) {
-            resultMessageId = R.string.correct_answer;
-        } else {
-            resultMessageId = R.string.incorrect_answer;
-        }
-        Toast.makeText(this, resultMessageId, Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(QUIZ_TAG, "Wywołana została metoda cyklu życia: onDestroy");
     }
 
 
-    private void setNextQuestion(){
-        questionTextView.setText(questions[currentIndex].getQuestionId());
-    }
+
+
+
 
 
 }
